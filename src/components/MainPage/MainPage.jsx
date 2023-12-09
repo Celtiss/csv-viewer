e265dc3a3a61a6f8658de797cd4f437f0e9dcceb
Elena Temirbekova <temn.lena@mail.ru>
2023-12-09 23:06:37 +0300

    feat: Добавить возможность добавления .csv файла, написать хук для парсинга файла

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './MainPage.module.scss';
 import FileButton from '../UI/FileButton/FileButton';
 import ErrToast from '../UI/ErrToast/ErrToast';
 
 function Main() {
+  const [isErr, setErrStatus] =useState(false);
+  const showErrToast = () => {
+    setErrStatus(true);
+    
+    // спустя три секунды убираем уведомление об ошибке
+    setTimeout(() => {
+      setErrStatus(false);
+    }, 3000);
+  };
+  
   return (
     <section className={styles.main}>
-      <ErrToast />
+      {isErr && <ErrToast />}
       <div className={styles.main__container}>
         <p className={styles.main__text}>Выберите файл в формате CSV</p>
-        <FileButton />
+        <FileButton showErrToast={showErrToast} />
       </div>
     </section>
   )
--- src/components/UI/FileButton/FileButton.jsx
+++ src/components/UI/FileButton/FileButton.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styles from './FileButton.module.scss';
+import useParserCsv from '../../../hooks/useParserCsv';
 
-function FileButton() {
+function FileButton({ showErrToast }) {
+  // Хук для парсинга csv файла
+  const { parcedFile, parseCsv, isParced } = useParserCsv();
+
+  // Ждем распарсенные данные, если все успешно, то сохраняем в localStorage
+  useEffect(() => {
+    if(isParced) {
+      localStorage.clear();
+      localStorage.setItem('csvFile',JSON.stringify(parcedFile));
+    }
+  }, [parcedFile, isParced]);
+
+  // Обработчик события добавления/изменения файла
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+
+    // Существует ли файл
+    if (file) {
+      // Проверка формата файла
+      const isCsv =  file.name.toLowerCase().endsWith('.csv');
+
+      // Если формат файла .csv, то читаем и парсим файл, иначе показываем ошибку
+      if (isCsv) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const csvFileText = e.target.result;
+          parseCsv(csvFileText);
+        };
+        reader.readAsText(file);
+      }
+      else {
+        showErrToast();
+      }
+    }
+  }
 
   return (
     <label className={styles.file}>
@@ -9,9 +44,10 @@ function FileButton() {
       <input
         className={styles.file__button}
         type='file'
-        accept='.csv'
+        // accept='.csv' // Закоментирован, чтобы проверить работоспособность уведомления о неправильном формате файла
         name='file'
         aria-label='file-button'
+        onChange={handleFileInputChange}
       />
     </label>
   );
